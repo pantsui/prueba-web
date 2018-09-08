10d22626573960a15ba8efa2e540ba9d37f8c39f
pantsui <43082170+pantsui@users.noreply.github.com>
2018-09-07 22:50:50 -0300

    Add files via upload

---
+++ php/pastoral.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<title>Colegio Arzobispo Crescente Errázuriz</title>
+	<link rel="shortcut icon" type="image/x-icon" href="../pictures/favicon.ico">	
+	<meta name="description" content="Pagina del Colegio Arzobispo Crescente Errázuriz">
+	<!--Metas-->
+	<meta charset="utf-8">
+	<meta name="googlebot" content="" >
+	<meta name="description" content="Esta es una institucion..." />
+	<!--metas-->
+	<link rel="stylesheet" type="text/css" href="../stylesheet/stylesheet.css">
+	<!--imagenes encabezado-->
+    <img src="../pictures/LOGOBE.png" width="250" height="50" title="Logo Fundacion Belen Educa" alt="LOGO FUNDACION BELEN EDUCA" class="logobelen">
+    <img align="right" src="../pictures/LTP.png" width="75" height="75" class="logotp">
+    <img align="left" src="../pictures/LogoCace.jpg" width="60" height="54" class="logocace">
+    <h1 class="CACE">Colegio Arzobispo Crescente Errazuriz</h1>
+    <p class="CACE">"En todo amar y servir"</p>
+	    <div id="header">
+	    <ul class="nav">
+	  	<li><a href="http://colegiocrecente.000webhostapp.com/#"> Inicio </a></li>
+			<li><a target="_blank"id="fundacion" href="http://www.beleneduca.cl"> Fundacion </a></li>
+	 		<li><a href="#">Primer Ciclo</a>
+					<ul>
+						<li><a href="../php/Pk.php">Pre-Kinder</a></li>
+						<li><a href="../php/kinder.php">Kinder</a></li>
+					    
+					</ul>
+			</li>
+			<li><a href ="#">Segundo Ciclo</a>
+					<ul>
+						<li><a href="../php/primero.php">1°Basico</a></li>
+				        <li><a href="../php/segundo.php">2°Basico</a></li>
+						<li><a href="../php/tercero.php">3°Basico</a></li>
+						<li><a href="../php/cuarto.php">4°Basico</a></li>
+						<li><a href="../php/quinto.php">5°Basico</a></li>
+						<li><a href="../php/sexto.php">6°Basico</a></li>
+						<li><a href="../php/septimo.php">7°Basico</a></li>
+						<li><a href="../php/octavo.php">8°Basico</a></li>
+					</ul>
+			</li>		
+					        
+					        
+            <li><a href ="#">Tercer Ciclo</a>
+					<ul>
+						<li><a href="1medio">I°Medio</a></li>  
+						<li><a href="2medio">II°Medio</a></li>
+						<li><a href="#">III°Medio</a>
+						 <ul>
+						        <li><a href="../php/3biologo.php">Biologo</a></li>
+					            <li><a href="../php/3humanista.php">Humanista</a></li>
+						        <li><a href="../php/3matematico.php">Matematico</a></li>
+						        <li><a href="../php/3tp.php">Tecnico Profesional</a></li>
+						  </ul>
+						</li>
+						<li><a href="#">IV°Medio</a>
+						 <ul>
+						        <li><a href="../php/4biologo.php">Biologo</a></li>
+					            <li><a href="../php/4humanista.php">Humanista</a></li>
+						        <li><a href="../php/4matematico.php">Matematico</a></li>
+						        <li><a href="../php/4tp.php">Tecnico Profesional</a></li>
+						  </ul>
+						</li>
+					</ul>
+			</li>
+				<li><a href="../php/pastoral.php">Pastoral</a></li>
+                <li><a href="../php/centroalumnos.php">Centro De Alumnos</a></li>
+                <li><a href="../php/actividades.php">Actividades</a></li>
+</ul>
+</div>
+</head>
+<body>
+<h1>Formacion y Pastoral</h1>
+ <p>La pastoral se dedica a la ayuda de los mas desfavorecidos, se hacen cosas como los trabajos de invierno y los trabajos de verano, en los que se viaja a lugares de chile donde se necesita ayuda monetaria, social, para dar una calida compañia a quien quiera y lo necesite.<br> Cualquiera se puede unir si se desea, solo se debe preguntar en recepcion por la profesora a cargo. <br> Actualmente es la profesora Sandra.</p>
+<img align="center" src="../pictures/pastoral.jpg"
+<br><br><br><br><br><br><br><br><br><br><br><br><br>
+<img src="../pictures/BB.jpg" width="100%" height="50%" >
+<div class="pie" style=" padding:10px 20px; background-color:#D32F2F;">
+	<p class="colegio"> 2018 - Colegio Arzobispo Crescente Errazuriz - Cabo de hornos, Puente Alto #1965, Santiago - Chile - Mesa central (2)25472627/(2)25472147 - Email:<a href="mailto:colegiocrescente@beleneduca.cl" class="pie">colegiocrecente@beleneduca.cl</a></p></div>
+</body>
+
+</html>
